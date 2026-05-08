44d9975a1b88d858b509e92a5ac82612164406f3
BlazeDashX <refat00021@gmail.com>
2026-05-09 03:36:14 +0600

    implement category filtering and dynamic plant card rendering from API

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 const categoryUrl = "https://openapi.programming-hero.com/api/categories";
 const allPlantUrl = "https://openapi.programming-hero.com/api/plants";
-// const plantByCategoryUrl = `https://openapi.programming-hero.com/api/category/${id}`;
+const plantByCategoryUrl = (id) =>
+    `https://openapi.programming-hero.com/api/category/${id}`;
 // const plantDetailsUrl = `https://openapi.programming-hero.com/api/plant/${id}`;
 
 const loadCategories = () => {
@@ -24,6 +25,7 @@ const displayCategories = (categories) => {
     allCategoryBtn.addEventListener("click", function (e) {
         e.preventDefault();
         setActive(this);
+        loadPlants();
     });
 
     categoryList.append(allCategory);
@@ -42,6 +44,7 @@ const displayCategories = (categories) => {
         catBtn.addEventListener("click", function (e) {
             e.preventDefault();
             setActive(this);
+            loadPlantsByCategory(category.id);
         });
 
         categoryList.append(li);
@@ -58,4 +61,77 @@ const setActive = (clickedBtn) => {
     clickedBtn.classList.add("active");
 };
 
+const loadPlants = () => {
+    fetch(allPlantUrl)
+        .then((res) => res.json())
+        .then((data) => displayPlants(data.plants));
+};
+
+const loadPlantsByCategory = (id) => {
+    fetch(plantByCategoryUrl(id))
+        .then((res) => res.json())
+        .then((data) => displayPlants(data.plants));
+};
+
+const displayPlants = (plants) => {
+    const plantCards = document.getElementById("plant-cards");
+
+    plantCards.innerHTML = "";
+
+    const cardContainer = document.createElement("div");
+
+    cardContainer.className = "grid grid-cols-3 gap-4";
+
+    plants.forEach((plant) => {
+        const card = document.createElement("div");
+
+        card.innerHTML = `
+            <div class="card bg-base-100 shadow-sm w-full">
+                <figure>
+                    <img
+                        class="w-full h-[220px] object-cover pt-5 px-5"
+                        src="${plant.image}"
+                        alt="${plant.name}"
+                    />
+                </figure>
+
+                <div class="p-4 space-y-3">
+                    <h2 class="card-title font-semibold text-sm">
+                        ${plant.name}
+                    </h2>
+
+                    <p class="text-xs text-[#1F2937] text-justify">
+                        ${plant.description.slice(0, 100)}...
+                    </p>
+
+                    <div class="card-actions justify-between">
+                        <div
+                            class="bg-[#ddfde7] text-green-700 rounded-3xl border-none"
+                        >
+                            <p class="py-1 px-3">
+                                ${plant.category}
+                            </p>
+                        </div>
+
+                        <div class="font-semibold text-base">
+                            <p>৳${plant.price}</p>
+                        </div>
+                    </div>
+
+                    <button
+                        class="bg-[#15813c] text-white rounded-3xl border-none hover:bg-white hover:text-black cursor-pointer w-full p-2"
+                    >
+                        Add To Cart
+                    </button>
+                </div>
+            </div>
+        `;
+
+        cardContainer.append(card);
+    });
+
+    plantCards.append(cardContainer);
+};
+
 loadCategories();
+loadPlants();
